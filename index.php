72bee046571fd77b14e5579674ce8a30782986ed
Benjamin B <bongage.benjamin@gmail.com>
2021-05-19 07:01:07 +0200

    Adding mains files.
    Uploading mains files of the projet on the repo.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    # Requires
+    namespace App\controller;
+    include_once 'vendor/autoload.php';
+
+    # Check if action is set, else = default
+    if(isset($_GET['action'])) {
+        $action = $_GET['action'];
+    } else {
+        $action = 'default'; # Action not set, so I set it to "default"
+    }
+
+    # Switch $action variable. If the action doesn't exist = default case
+    switch($action) {
+        # DECT Section
+        
+        # TBT Section
+        
+        # Default page if the action is not valid
+        default:
+            print('default');
+            break;
+    }
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'benjamin/dalkia_dect_tbt',
+  ),
+  'versions' => 
+  array (
+    'benjamin/dalkia_dect_tbt' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
